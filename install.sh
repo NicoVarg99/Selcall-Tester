3a9b7f122098b2c9c759708345e5174722130766
Nicola Salsotto <nicola@cryptolab.net>
2016-11-16 19:52:47 +0000

    Add download and installation scripts.

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get install sox
+sudo apt-get install make gcc g++
+cd
+git clone https://github.com/Starwarsfan2099/fm_transmitter
+cd fm_transmitter/
+sudo make
+
---
+++ quick.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd
+sudo apt-get install git
+git clone https://github.com/NicoVarg99/Selcall-Tester
+cd Selcall-Tester
+./install.sh
